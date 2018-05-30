0c6e62154abd105b8b4d72b6c1bbe07d5e8cbc49
Antonio Vanegas <hpsaturn@gmail.com>
2018-05-30 21:15:38 +0000

    renamed to server and test custom gatt library

--- server.py
+++ server.py
@@ -4,18 +4,52 @@ import dbus
 import dbus.exceptions
 import dbus.mainloop.glib
 import dbus.service
-import gattbase
 
 import array
+
 try:
   from gi.repository import GObject
 except ImportError:
   import gobject as GObject
-import sys
 
-from random import randint
+import sys
+from gatt import *
 
 mainloop = None
+
+class Application(dbus.service.Object):
+    """
+    org.bluez.GattApplication1 interface implementation
+    """
+    def __init__(self, bus):
+        self.path = '/'
+        self.services = []
+        dbus.service.Object.__init__(self, bus, self.path)
+        self.add_service(WifiConfigService(bus, 0))
+
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
+
+    def add_service(self, service):
+        self.services.append(service)
+
+    @dbus.service.method(DBUS_OM_IFACE, out_signature='a{oa{sa{sv}}}')
+    def GetManagedObjects(self):
+        response = {}
+        print('GetManagedObjects')
+
+        for service in self.services:
+            response[service.get_path()] = service.get_properties()
+            chrcs = service.get_characteristics()
+            for chrc in chrcs:
+                response[chrc.get_path()] = chrc.get_properties()
+                descs = chrc.get_descriptors()
+                for desc in descs:
+                    response[desc.get_path()] = desc.get_properties()
+
+        return response
+
+
 
 class WifiConfigService(Service):
     """
