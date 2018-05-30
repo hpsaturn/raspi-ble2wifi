95c22f7139ce93e86ee7a1cc705aa7db34ef53a7
Antonio Vanegas <hpsaturn@gmail.com>
2018-05-30 02:13:16 -0500

    testing wifi characteristic and ble on/off service

--- gatt-server.py
+++ gatt-server.py
@@ -259,7 +259,6 @@ class WifiConfigService(Service):
         self.add_characteristic(WifiCharacteristic(bus, 0, self))
         self.add_characteristic(WifiEncryptCharacteristic(bus, 1, self))
         self.add_characteristic(WifiSecureCharacteristic(bus, 2, self))
-        self.energy_expended = 0
 
 class WifiCharacteristic(Characteristic):
     """
@@ -273,54 +272,19 @@ class WifiCharacteristic(Characteristic):
         Characteristic.__init__(
                 self, bus, index,
                 self.WIFI_CHRC_UUID,
-                ['notify','read', 'write', 'writable-auxiliaries'],
+                ['read', 'write', 'writable-auxiliaries'],
                 service)
         self.value = []
         self.add_descriptor(WifiDescriptor(bus, 0, self))
         self.add_descriptor(CharacteristicUserDescriptionDescriptor(bus, 1, self))
-        self.notifying = False
-        self.battery_lvl = 100
-        GObject.timeout_add(5000, self.drain_battery)
-
-    def notify_battery_level(self):
-        if not self.notifying:
-            return
-        self.PropertiesChanged(
-                GATT_CHRC_IFACE,
-                { 'Value': [dbus.Byte(self.battery_lvl)] }, [])
-
-    def drain_battery(self):
-        if self.battery_lvl > 0:
-            self.battery_lvl -= 2
-            if self.battery_lvl < 0:
-                self.battery_lvl = 0
-        print('Battery Level drained: ' + repr(self.battery_lvl))
-        self.notify_battery_level()
-        return True
 
     def ReadValue(self, options):
-        print('Battery Level read: ' + repr(self.battery_lvl))
-        return [dbus.Byte(self.battery_lvl)]
+        print('WifiCharacteristic Write: ' + repr(self.value))
+        return [dbus.Byte(self.value)]
 
     def WriteValue(self, value, options):
         print('WifiCharacteristic Write: ' + repr(value))
         self.value = value
-
-    def StartNotify(self):
-        if self.notifying:
-            print('Already notifying, nothing to do')
-            return
-
-        self.notifying = True
-        self.notify_battery_level()
-
-    def StopNotify(self):
-        if not self.notifying:
-            print('Not notifying, nothing to do')
-            return
-
-        self.notifying = False
-
 
 class WifiDescriptor(Descriptor):
     """
